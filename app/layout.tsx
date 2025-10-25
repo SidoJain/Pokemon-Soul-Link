df7ce47c2a7528af91c76c487ac8ab261e947bd8
Siddharth Jain <sidojain30705@gmail.com>
2025-10-25 12:30:39 +0530

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Suspense } from "react"
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
@@ -31,6 +32,7 @@ export default function RootLayout({
                     <Suspense fallback={null}>{children}</Suspense>
                 </ThemeProvider>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
