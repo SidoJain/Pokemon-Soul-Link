b8e2631c50ba4bb2a6e16e2e77659e71a1ffa3e5
Siddharth Jain <sidojain30705@gmail.com>
2025-10-25 12:04:21 +0530

    add persistence

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -19,7 +19,13 @@ export async function updateSession(request: NextRequest) {
                     supabaseResponse = NextResponse.next({
                         request,
                     })
-                    cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options))
+                    cookiesToSet.forEach(({ name, value, options }) => {
+                        const extendedOptions = {
+                            ...options,
+                            maxAge: 60 * 60 * 24 * 7,
+                        }
+                        supabaseResponse.cookies.set(name, value, extendedOptions)
+                    })
                 },
             },
         },
